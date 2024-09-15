d05b8e82da24b2a12511c15be52b2a7dea95f38a
Aman <thakuraman122004@gmail.com>
2024-09-16 01:34:40 +0530

    UPLOAD

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,21 +1,16 @@
 import { RouterProvider, createBrowserRouter, Route, createRoutesFromElements } from "react-router-dom";
 import Layout from "./layout/Layout";
 import NotFound from "./layout/NotFound";
-import Landing from "./components/Landing";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import About from "./components/About";
 import ChatBot from "./components/ChatBot";
 import Faq from "./components/Faq";
 import Translator from "./components/Translator";
-// import Redirect from "./components/Redirect"; // If you have a Redirect component
 
-// Define routes using createRoutesFromElements for better modularity
 const routes = createRoutesFromElements(
   <>
-    {/* <Route path="/" element={<Redirect to="/home" />} /> */}
     <Route path="/" element={<Home  />} />
-    {/* <Route path="/home" element={<Home />} /> */}
     <Route path="/about" element={<About />} />
     <Route path="/chat" element={<ChatBot />} />
     <Route path="/faqs" element={<Faq />} />
